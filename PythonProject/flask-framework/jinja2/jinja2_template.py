5a3d775dc8f7342ea6437f7ee3ff2cc79916b94b
Ramesh Babu <mr.vrbabu@hotmail.com>
2025-03-25 04:40:14 +0000

    Adding new html and jinja files

--- PythonProject/flask-framework/jinja2/jinja2_template.py
+++ PythonProject/flask-framework/jinja2/jinja2_template.py
@@ -1,3 +1,12 @@
+# Variable Rule 
+
+# Jinja template 
+
+'''
+{{ }} expression to print out in html 
+{% .... %} Conditions and loops 
+{#... #} This is for comments
+'''
 from flask import Flask, render_template, request
 
 getpost = Flask(__name__)
@@ -13,22 +22,50 @@ def about():
     return render_template('about.html')
 
 
-# @getpost.route('/form', methods=['GET', 'POST'])
-# def postform():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         return f"Welcome {name} !"
-#     return render_template('form.html')
-
-
 @getpost.route('/submit', methods=['GET', 'POST'])
-def form():
+def submit():
     if request.method == 'POST':
         name = request.form['name']
-        return None
-        # return f"Welcome {name} !"
+        return f"Welcome {name} !"
     return render_template('form.html')
+
+# Variable Rule 
+
+@getpost.route('/success/<score>')
+def success(score):
+    return "You have scored " + score
+
+
+# Jinja2 Template 
+
+@getpost.route('/marks/<int:myscore>')
+def marks(myscore):
+    res=""
+    if myscore >= 50:
+        res="PASSED"
+    else:
+        res="FAILED"
+        
+    return render_template('results.html', results=res)
+
+
+@getpost.route('/successres/<int:myscore>')
+def successres(myscore):
+    res=""
+    if myscore >= 50:
+        res="PASSED"
+    else:
+        res="FAILED"
+        
+    exp = {'score': myscore, "res": res}
+        
+    return render_template('myresults.html', results=exp)
+
+# if else condition 
+@getpost.route('/successif/<int:newscore>')
+def successif(newscore):
+    return render_template('successifi.html', newscore=newscore)
 
 
 if __name__ == "__main__":
-    getpost.run(debug=True, host='0.0.0.0', port='5002')
+    getpost.run(debug=True, host='0.0.0.0', port='5000')
